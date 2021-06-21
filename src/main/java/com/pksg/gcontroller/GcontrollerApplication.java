e5635586e257a566906cc27fdeb665555e6936e1
praveen787 <praveenyadav19@gmail.com>
2021-06-22 00:53:44 +0530

    first commit

---
+++ src/main/java/com/pksg/gcontroller/GcontrollerApplication.java
@@ -0,0 +1,16 @@
+package com.pksg.gcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class GcontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcontrollerApplication.class, args);
+	}
+	 
+
+
+}
